f7147c00964334fce6b29b576b8950db5770d870
Isaac Tullis <isaacjtullis@gmail.com>
2016-10-10 21:04:53 -0600

    user registration tests finished

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -20,6 +20,25 @@ feature 'sign up', %Q{
     expect(page).to have_content("Sign Out")
   end
 
-  scenario 'required information is not supplied'
-  scenario 'password confirmation does not match configuration'
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+  scenario 'password confirmation does not match configuration' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirmation', with: 'wrongpassword'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match Password")
+    expect(page).to_not have_content("Sign Out")
+  end
 end
