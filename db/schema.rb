fcbbdcabd5a1c65c5161e009d2a1ad4bb265e56c
Isaac Tullis <isaacjtullis@gmail.com>
2016-11-27 16:58:07 -0700

    Made a table and deleted a column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126235208) do
+ActiveRecord::Schema.define(version: 20161127192912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
